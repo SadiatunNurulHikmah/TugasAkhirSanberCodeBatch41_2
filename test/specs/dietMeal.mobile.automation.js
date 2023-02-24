4c226a6737e9605b3f20564839c3a5615b86de18
SadiatunNurulHikmah <diah.hikmah@gmail.com>
2023-02-24 14:11:16 +0700

    add script dietmeal.mobile.auto.js

---
+++ test/specs/dietMeal.mobile.automation.js
@@ -0,0 +1,51 @@
+
+
+describe('Open Diet Meal Application', function() {
+    it('should open Diet Meal app', async () => {
+        const welcome = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView[1]');
+        
+        // assert test Welcome... di halaman welcome page
+        await expect(welcome).toHaveAttrContaining('text', 'Welcome...');
+    })
+
+    it('input data in welcome page', async () => {
+        // input Name
+        await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.EditText').setValue('Diah');
+        
+        // input weight
+        await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.widget.EditText').setValue('47');
+        
+        // input height
+        await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[3]/android.widget.FrameLayout/android.widget.EditText').setValue('163');
+        
+        // choose female
+        await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RadioGroup/android.widget.RadioButton[2]').click();
+        
+        // click button next
+        await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.Button').click();
+
+    })
+
+    it('should open choose daily activities page', async () => {
+        const dailyActivities = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView[2]');
+        
+        // assert test Aktivitas sehari-hari di halaman choose daily activities page
+        await expect(dailyActivities).toHaveAttrContaining('text', 'Aktivitas sehari-hari');
+    })
+
+    it('choose daily activities', async () => {
+        // choose sekolah, kuliah, kerja kantor, dsj.
+        await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RadioGroup/android.widget.RadioButton[4]').click();
+        
+        // click button selesai
+        await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.Button').click();
+        
+    })
+
+    it('should open home page', async () => {
+        const home = await $('//android.widget.FrameLayout[@content-desc="Home"]/android.widget.ImageView');
+        
+        // assert selected true di halaman home page
+        await expect(home).toHaveAttrContaining('selected', true);
+    })
+})
